20cee66d8df8b00dda95950637c30dbeeff5d4d7
leoyoung02 <flarelight0125@gmail.com>
2023-02-26 07:30:09 +0300

    Connection test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,29 @@
 require('dotenv').config();
+const { Client } = require('pg');
+
+const connectionData = {
+  user: process.env.db_user,
+  host: process.env.db_host,
+  database: process.env.db_name,
+  password: process.env.db_password,
+  port: process.env.db_port,
+}
+
+console.log("Connection : ")
+
+console.log(connectionData)
+
+const connection = new Client({
+  user: process.env.db_user,
+  host: process.env.db_host,
+  database: process.env.db_name,
+  password: process.env.db_password,
+  port: process.env.db_port,
+});
+
+
+console.log(connection)
+
 const http = require('http')
 const { connectionResult } = require('./database/connect')
 
