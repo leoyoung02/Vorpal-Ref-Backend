a4330e616cc7ef910932123bb0e13db07b2c7574
leoyoung02 <flarelight0125@gmail.com>
2023-02-26 08:28:21 +0300

    Connection fix

--- src/index.js
+++ src/index.js
@@ -10,6 +10,17 @@ const connection = new Client({
   port: process.env.db_port,
 });
 
+const connectionResult = connection.connect((err, res) => {
+  if (res) {
+      console.log("Res : ")
+      console.log(res)
+  }
+  if (err) {
+      console.log("Err : ")
+      console.log(err)
+  }
+})
+
 async function testQuery () {
   const sqlQuery = "select * from address_to_referral limit 1;"
   const queryResult = connection.query(sqlQuery, (err, res) => {
