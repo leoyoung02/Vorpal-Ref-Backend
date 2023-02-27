adeda607aef8b3b405e9068dfadab2ec6a422615
leoyoung02 <flarelight0125@gmail.com>
2023-02-27 06:47:39 +0300

    Connection test

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const http = require('http')
 
 const connectionData = {
   user: process.env.db_user,
-  host: 'localhost',
+  host: process.env.db_host,
   database: process.env.db_name,
   password: process.env.db_password, //process.env.db_password,
   port: process.env.db_port,
