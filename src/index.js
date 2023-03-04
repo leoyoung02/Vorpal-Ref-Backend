15a6db88a2d99d0f2a8034ef9138a0d6927926fe
leoyoung02 <flarelight0125@gmail.com>
2023-03-04 18:49:55 +0300

    DB links query test fix

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ const port = process.argv[2] ? process.argv[2] : process.env.default_port
 
 async function tests () {
   console.log("Tests : ")
-  console.log(await DBMigration())
-  // console.log(await AddNewLink('0xDD099d768d18E9a6b0bd9DFa02A5FD3A840a273f', 10, 20))
-  // console.log(await RegisterReferral('0xAE8A7aC2358505a11f51c7a1C1522D7b95Afe66F', 'ac21766476906b650f7502530a796f19'))
-  // console.log(await GetLinksByOwner('0xDD099d768d18E9a6b0bd9DFa02A5FD3A840a273f'))
+  // console.log(await DBMigration())
+  console.log(await AddNewLink('0xDD099d768d18E9a6b0bd9DFa02A5FD3A840a273f', 10, 20))
+  console.log(await RegisterReferral('0xAE8A7aC2358505a11f51c7a1C1522D7b95Afe66F', 'ac21766476906b650f7502530a796f19'))
+  console.log(await GetLinksByOwner('0xDD099d768d18E9a6b0bd9DFa02A5FD3A840a273f'))
 }
 
 tests()
