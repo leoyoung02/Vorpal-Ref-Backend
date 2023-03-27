9da27aeb1bec473412475b8558faf90a52dcd99d
leoyoung02 <flarelight0125@gmail.com>
2023-03-27 13:05:13 +0300

    Configs

---
+++ src/config.js
@@ -0,0 +1,7 @@
+const networkId = 97
+const networkData = {}
+export const chainName = 'Binance'
+// export const ethSymbol = 'BNB'
+export const ethSymbol = 'BNB'
+export const rpcUrl = "https://data-seed-prebsc-1-s1.binance.org:8545/"
+
