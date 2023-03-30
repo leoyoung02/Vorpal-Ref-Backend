05b8aa1ab1355f693bdae5ee2dc846d63cb719e7
leoyoung02 <flarelight0125@gmail.com>
2023-03-30 10:22:53 +0300

    Config update

--- src/blockchain/config.js
+++ src/blockchain/config.js
@@ -1,6 +1,7 @@
 const chain = {
     chainId: 97,
     rpc: 'https://data-seed-prebsc-1-s1.binance.org:8545/',
+    startBlock: 28489906,
     contracts: [],
     saleTokens: [],
     paymentTokens: []
