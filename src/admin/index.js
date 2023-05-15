f4da43969c7f47aa4ceaac740bbfad6dd369cf83
leoyoung02 <flarelight0125@gmail.com>
2023-05-15 10:58:03 +0300

    log function

--- src/admin/index.js
+++ src/admin/index.js
@@ -1,6 +1,7 @@
 const { connection } = require('../database/connection')
 const Web3 = require('web3');
 const { sha256 } = require('sha256')
+const { WriteLog } = require('./log')
 
 /* In progress */
 
@@ -11,6 +12,28 @@ function GenerateAuthMessage ( msgtext = 'getcontent_' ) {
    // console.log(msgstring) 
     const hash = sha256(msgstring)
     return hash;
+}
+
+async function CheckRights ( signature, msgtext = 'getcontent_' ) {
+
+    if ( !signature ) {
+        return null;
+    }
+
+    const web3 = new Web3(config.rpcUrl)
+    const msg = GenerateAuthMessage ( msgtext )
+    const request_address = web3.eth.accounts.recover(msg, signature).toLowerCase()
+    
+    const admins_query = `SELECT address FROM users WHERE rights = 'admin' AND address = '${request_address}';`;
+
+    const user_query = await connection.query(admins_query);
+
+    if (user_query.rows.length === 0) {
+        WriteLog(request_address, "Auth failed")
+        return null
+    }
+    WriteLog(user_query.rows[0].address, "Auth success")
+    return user_query.rows[0].address
 }
 
 async function RequestAdminData ( request ) {
---
+++ src/admin/log.js
@@ -0,0 +1,21 @@
+const FS = require('fs')
+
+
+function WriteLog ( address, message ) {
+    const time = new Date()
+    const year = date.getFullYear();
+    const month = ('0' + (date.getMonth() + 1)).slice(-2);
+    const day = ('0' + date.getDate()).slice(-2);
+    const hours = ('0' + date.getHours()).slice(-2);
+    const minutes = ('0' + date.getMinutes()).slice(-2);
+    const seconds = ('0' + date.getSeconds()).slice(-2);
+
+    const TimeMark =`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
+
+    const log = `${TimeMark} ${address} ${message}`
+    return log
+}
+
+module.exports = {
+    WriteLog
+}
