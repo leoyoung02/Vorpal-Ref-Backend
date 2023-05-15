4c451e2adbe1c15451c7ff35ce7fb90dbf49c26c
leoyoung02 <flarelight0125@gmail.com>
2023-05-15 13:23:59 +0300

    fix

--- src/admin/log.js
+++ src/admin/log.js
@@ -4,12 +4,12 @@ const filePath = '../../../admin.log';
 
 function WriteLog ( address, message ) {
     const time = new Date()
-    const year = date.getFullYear();
-    const month = ('0' + (date.getMonth() + 1)).slice(-2);
-    const day = ('0' + date.getDate()).slice(-2);
-    const hours = ('0' + date.getHours()).slice(-2);
-    const minutes = ('0' + date.getMinutes()).slice(-2);
-    const seconds = ('0' + date.getSeconds()).slice(-2);
+    const year = time.getFullYear();
+    const month = ('0' + (time.getMonth() + 1)).slice(-2);
+    const day = ('0' + time.getDate()).slice(-2);
+    const hours = ('0' + time.getHours()).slice(-2);
+    const minutes = ('0' + time.getMinutes()).slice(-2);
+    const seconds = ('0' + time.getSeconds()).slice(-2);
 
     const TimeMark =`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 
