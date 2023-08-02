1878c0acb0fab93e5f966d75e516a787c3b3e255
leoyoung02 <flarelight0125@gmail.com>
2023-08-02 10:25:58 +0500

    socket msg testing fix

--- src/game/io/ioServer.ts
+++ src/game/io/ioServer.ts
@@ -1,34 +1,37 @@
 import WebSocket, { Server } from 'ws';
-import { createServer } from 'http';
 import { default_ws_port, pingPongDelay } from '../config';
 import { WriteLog } from '../../database/log';
 
 interface WSRow {
-        id: string,
-        ws: WebSocket
-  }
+  id: string;
+  ws: WebSocket;
+}
 
 export class GameIoServer {
   private players: WSRow[] = [];
   private ws_port = Number(
     process.env.WS_PORT ? process.env.WS_PORT : default_ws_port,
   );
-  private wss = new WebSocket.Server({ port: this.ws_port });
-    private timer: any;
-    
- private GenerateNewId() {
-      return String(Math.round(Math.random() * 1000000000));
+  // private wss = new WebSocket.Server({ port: this.ws_port });
+  private timer: any;
+
+  private GenerateNewId() {
+    return String(Math.round(Math.random() * 1000000000));
   }
 
   public Start() {
-      this.wss.on('connection', (ws: WebSocket) => {
-        const cId = this.GenerateNewId();
-          this.players.push({
-              id: cId,
-              ws: ws
-          });
-          WriteLog('0x0032', 'New connection, id : ' + cId + ' ws : ' + JSON.stringify(ws));
-      ws.send('Connection confirmed')
+    const wss = new WebSocket.Server({ port: this.ws_port });
+    wss.on('connection', (ws: WebSocket) => {
+      const cId = this.GenerateNewId();
+      this.players.push({
+        id: cId,
+        ws: ws,
+      });
+      WriteLog(
+        '0x0032',
+        'New connection, id : ' + cId,
+      );
+      ws.send('Connection confirmed');
       ws.on('message', (message: string) => {
         if (message === 'ping') {
           ws.send('pong');
