b532592daf9374f1552a8cac4f5df93346c77dc8
leoyoung02 <flarelight0125@gmail.com>
2023-08-02 10:47:53 +0500

    socket msg testing, update

--- src/game/io/ioServer.ts
+++ src/game/io/ioServer.ts
@@ -1,41 +1,75 @@
 import WebSocket, { Server } from 'ws';
 import { default_ws_port, pingPongDelay } from '../config';
 import { WriteLog } from '../../database/log';
+import { PlayerState } from '../types';
 
-interface WSRow {
+interface Player {
   id: string;
   ws: WebSocket;
+  publicKey: string;
+}
+interface PlayerRow extends Player {
+  state: PlayerState;
 }
 
 export class GameIoServer {
-  private players: WSRow[] = [];
+  private players: PlayerRow[] = [];
   private ws_port = Number(
     process.env.WS_PORT ? process.env.WS_PORT : default_ws_port,
   );
   // private wss = new WebSocket.Server({ port: this.ws_port });
   private timer: any;
+  private playerDefaultState: PlayerState = {
+    auth: true,
+    inLookingFor: false,
+    inGame: false,
+    starId: -1,
+    planetId: -1,
+    roomId: -1,
+  };
 
-  private GenerateNewId() {
+  private GenerateId(): string {
     return String(Math.round(Math.random() * 1000000000));
+  }
+
+  private InsertPlayer(player: Player): boolean {
+    this.players.push({
+      id: player.id,
+      ws: player.ws,
+      publicKey: player.publicKey,
+      state: this.playerDefaultState,
+    });
+    return true;
+  }
+
+  private DeletePlayer(id: string) {
+    const newPlayerList: PlayerRow[] = [];
+    this.players.forEach((player) => {
+      if (player.id !== id) {
+        newPlayerList.push(player);
+      }
+    });
+    this.players = newPlayerList;
   }
 
   public Start() {
     const wss = new WebSocket.Server({ port: this.ws_port });
     wss.on('connection', (ws: WebSocket) => {
-      const cId = this.GenerateNewId();
-      this.players.push({
+      const cId = this.GenerateId();
+      this.InsertPlayer({
         id: cId,
         ws: ws,
+        publicKey: '',
       });
-      WriteLog(
-        '0x0032',
-        'New connection, id : ' + cId,
-      );
+      WriteLog('0x0032', 'New connection, id : ' + cId);
       ws.send('Connection confirmed');
       ws.on('message', (message: string) => {
         if (message === 'ping') {
           ws.send('pong');
         }
+      });
+      ws.on('close', () => {
+        this.DeletePlayer(cId);
       });
     });
     this.timer = setInterval(() => {
