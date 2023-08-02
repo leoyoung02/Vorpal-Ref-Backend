1ffeb6510c4d81b90db0720a5842ba3c5cc113cd
leoyoung02 <flarelight0125@gmail.com>
2023-08-02 13:02:05 +0500

    players, in progress

--- src/game/io/ioServer.ts
+++ src/game/io/ioServer.ts
@@ -42,6 +42,46 @@ export class GameIoServer {
     return 'auth_' + String(dt - (dt % 600000));
   }
 
+  private SelectIndexes(max: number): number[] {
+    const indexes: number[] = [];
+    const indexOne = Math.floor(Math.random() * (max + 1));
+    indexes.push(indexOne);
+    while (true) {
+      const indexTwo = Math.floor(Math.random() * (max + 1));
+      if (indexTwo !== indexOne) {
+        indexes.push(indexTwo);
+        break;
+      }
+    }
+    return indexes;
+  }
+
+  private GetPlayerByParam (param: WebSocket | string, value: string): PlayerRow | null {
+    this.players.forEach((player) => {
+      if (player.id === param || player.ws === param || player.publicKey === param) {
+        return player;
+      }
+    });
+    return null;
+  }
+
+  private UpdatePlayerState(id: string, state: PlayerState) {
+    const newPlayerList: PlayerRow[] = [];
+    this.players.forEach((player) => {
+      if (player.id !== id) {
+        newPlayerList.push(player);
+      } else {
+        newPlayerList.push({
+          id: player.id,
+          ws: player.ws,
+          publicKey: player.publicKey,
+          state: state,
+        });
+      }
+    });
+    this.players = newPlayerList;
+  }
+
   private InsertPlayer(player: Player): boolean {
     this.players.push({
       id: player.id,
@@ -72,46 +112,46 @@ export class GameIoServer {
       WriteLog('0x0032', 'New connection, id : ' + cId);
       ws.send(JSON.stringify(this.AuthRequestMsg));
       ws.on('message', (message: string) => {
-        WriteLog('0x0033', 'Received : ' + message);
         if (String(message) === 'ping') {
           ws.send('pong');
           return;
         }
+        WriteLog('0x0033', 'Received : ' + message);
         let msg: any;
         try {
           msg = JSON.stringify(message);
         } catch (e) {
           return;
-          }
-          switch (msg.action) {
-              case actionList.auth:
-                  if (msg.signature) return;
-                  const recoverMsg = this.AuthMsg();
-                  const publicKey = web3.eth.accounts
-                    .recover(recoverMsg, msg.signature)
-                      .toLowerCase();
-                  this.players.forEach((player) => {
-                      if (player.publicKey === publicKey) {
-                          ws.send(
-                            JSON.stringify({
-                              action: actionList.unauth,
-                              message:
-                                'Auth failed, player with this key is already online',
-                            }),
-                          );
-                          return;
-                      }
-                  })
-                  clearInterval(authTimer)
-                        this.InsertPlayer({
-                          id: cId,
-                          ws: ws,
-                          publicKey: publicKey,
-                        });
-                  break;
-              default:
-                  return;
-          }
+        }
+        switch (msg.action) {
+          case actionList.auth:
+            if (msg.signature) return;
+            const recoverMsg = this.AuthMsg();
+            const publicKey = web3.eth.accounts
+              .recover(recoverMsg, msg.signature)
+              .toLowerCase();
+            this.players.forEach((player) => {
+              if (player.publicKey === publicKey) {
+                ws.send(
+                  JSON.stringify({
+                    action: actionList.unauth,
+                    message:
+                      'Auth failed, player with this key is already online',
+                  }),
+                );
+                return;
+              }
+            });
+            clearInterval(authTimer);
+            this.InsertPlayer({
+              id: cId,
+              ws: ws,
+              publicKey: publicKey,
+            });
+            break;
+          default:
+            return;
+        }
       });
       ws.on('close', () => {
         this.DeletePlayer(cId);
