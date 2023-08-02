148606a1d2e74dc37b8ab3db90c07a8f597b93fa
leoyoung02 <flarelight0125@gmail.com>
2023-08-02 10:09:48 +0500

    socket msg testing

--- src/game/io/ioServer.ts
+++ src/game/io/ioServer.ts
@@ -1,42 +1,51 @@
-import io, {Server, Socket} from 'socket.io';
+import WebSocket, { Server } from 'ws';
 import { createServer } from 'http';
 import { default_ws_port, pingPongDelay } from 'game/config';
 import { WriteLog } from 'database/log';
 
+interface WSRow {
+        id: string,
+        ws: WebSocket
+  }
+
 export class GameIoServer {
-  private players: Socket[] = [];
-  private timer: any;
+  private players: WSRow[] = [];
   private ws_port = Number(
     process.env.WS_PORT ? process.env.WS_PORT : default_ws_port,
   );
+  private wss = new WebSocket.Server({ port: this.ws_port });
+    private timer: any;
+    
+ private GenerateNewId() {
+      return String(Math.round(Math.random() * 1000000000));
+  }
 
   public Start() {
-    const httpServer = createServer();
-    const io = new Server(httpServer, {
-      // options
-    });
-    httpServer.listen(this.ws_port);
-    io.on('connection', (ws) => {
-      this.players.push(ws);
-        WriteLog('0x0032', 'New connection, id : ' + ws.id);
-        ws.on('message', (message: string) => {
-            if (message === 'ping') {
-                ws.send('pong')
-            }
-        })
+      this.wss.on('connection', (ws: WebSocket) => {
+        const cId = this.GenerateNewId();
+          this.players.push({
+              id: cId,
+              ws: ws
+          });
+      WriteLog('0x0032', 'New connection, id : ' + cId + ' ws : ' + JSON.stringify(ws));
+      ws.on('message', (message: string) => {
+        if (message === 'ping') {
+          ws.send('pong');
+        }
+      });
     });
     this.timer = setInterval(() => {
-        this.players.forEach((player) => {
-           player.send('pong')
-       })
+      this.players.forEach((player) => {
+        player.ws.send('pong');
+      });
     }, pingPongDelay);
     return true;
   }
 
-    public Finish() {
-        if (this.timer) {
-          clearInterval(this.timer)
-      }
+  public Finish() {
+    if (this.timer) {
+      clearInterval(this.timer);
+    }
     return true;
   }
 }
