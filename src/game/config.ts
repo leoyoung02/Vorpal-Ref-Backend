efd720b36827cd6630d745a20571a80285093a30
leoyoung02 <flarelight0125@gmail.com>
2023-08-27 18:38:45 +0500

    Config timer update

--- src/game/config.ts
+++ src/game/config.ts
@@ -1,6 +1,6 @@
 export const signTimeout = 120000;
-export const roomTestTimeout = 10000;
-export const gameTimerValue = 30000;
+export const roomTestTimeout = 30000;
+export const gameTimerValue = 15000;
 export const pingPongDelay = 3000;
 export const default_ws_port = 3078;
 export const gameField = [800, 1400];
