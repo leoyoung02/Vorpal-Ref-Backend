e50f954d11cc01398d99e2cd6ba1acd2ef141fb9
leoyoung02 <flarelight0125@gmail.com>
2023-11-20 16:53:32 +0500

    function log

--- src/game/gameplay/GameObject.ts
+++ src/game/gameplay/GameObject.ts
@@ -67,7 +67,7 @@ export default abstract class GameObject {
     this.inMoving = false;
     const logMsg = {
       action: actionList.log,
-      ...onFinish
+      onfinish: String(onFinish)
     }
     this.room.ReSendMessage(JSON.stringify(logMsg));
     if (onFinish) onFinish(this.id, this.center);
