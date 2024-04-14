45c37d0bcd0531a17f7d43aea367147338212d20
leoyoung02 <flarelight0125@gmail.com>
2024-04-15 04:50:25 +0500

    box opening fix test

--- src/database/rewards/index.ts
+++ src/database/rewards/index.ts
@@ -114,7 +114,7 @@ export async function OpenBox(boxId: number) {
   const logQuery = `INSERT INTO box_log (boxId, opening, openResult)
     VALUES (${boxId}, ${Math.round(
     new Date().getTime() / 1000,
-  )}, '${rewardType}', ${openAmount});`;
+  )}, '${rewardType}');`;
   const balanceQuery = `UPDATE resources SET ${rewardType} = ${rewardType} + ${openAmount} 
   WHERE ownerAddress IN (SELECT ownerAddress FROM boxes WHERE id = ${boxId})`;
   await connection.query(logQuery);
