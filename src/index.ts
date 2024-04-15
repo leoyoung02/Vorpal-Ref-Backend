faae7424c4f3fae7dcb48a62e88829dfd0bb0783
leoyoung02 <flarelight0125@gmail.com>
2024-04-15 17:16:29 +0500

    Box api functions fix

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,24 @@
-import { GiveResources } from "database/rewards";
-import { StartWatchingTimer } from "./blockchain/Stars/watcher";
-import { InitGameIoServer } from "./game";
-import { AdminDataRequest, AdminSaveData, AdminUpdateUserData, CreateBox, GetAdminUserData, GetAllStars, GetLinksByOwnerResponse, GetOwnerDataResponse, GetProjectData, GetUserAvailableBoxes, GetUserResources, OpenBoxRequest, ReferralApiDefault, UpdateAllStars, UpdateOneStar, WithdrawRewardAction } from "./responces";
+import { StartWatchingTimer } from './blockchain/Stars/watcher';
+import { InitGameIoServer } from './game';
+import {
+  AdminDataRequest,
+  AdminSaveData,
+  AdminUpdateUserData,
+  CreateBox,
+  GetAdminUserData,
+  GetAllStars,
+  GetLinksByOwnerResponse,
+  GetOwnerDataResponse,
+  GetProjectData,
+  GetUserAvailableBoxes,
+  GetUserResources,
+  GiveResourcesResponce,
+  OpenBoxRequest,
+  ReferralApiDefault,
+  UpdateAllStars,
+  UpdateOneStar,
+  WithdrawRewardAction,
+} from './responces';
 const dEnv = require('dotenv');
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -9,21 +26,23 @@ const app = express();
 
 dEnv.config();
 
-const port = process.argv[2] ? process.argv[2] : process.env.DEFAULT_PORT
+const port = process.argv[2] ? process.argv[2] : process.env.DEFAULT_PORT;
 
 // End boxes
 
 app.use(express.json());
 
-app.use(bodyParser.urlencoded({
-  extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  }),
+);
 
 app.get('/', (req, res) => {
   res.status(200).send('API homepage');
-})
+});
 
-   /*
+/*
    res.setHeader("Access-Control-Allow-Origin", "*" );
    res.setHeader('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE");
    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Accept');
@@ -31,42 +50,42 @@ app.get('/', (req, res) => {
    */
 
 // Boxes
-app.post('/api/boxes/create', CreateBox)
+app.post('/api/boxes/create', CreateBox);
 
-app.post('/api/boxes/open', OpenBoxRequest)
+app.post('/api/boxes/open', OpenBoxRequest);
 
-app.post('/api/boxes/available', GetUserAvailableBoxes)
+app.post('/api/boxes/available', GetUserAvailableBoxes);
 
-app.post('/api/boxes/assets/give', GiveResources)
+app.post('/api/boxes/assets/give', GiveResourcesResponce);
 
-app.post('/api/boxes/assets', GetUserResources)
+app.post('/api/boxes/assets', GetUserResources);
 
 // Referral
-app.post('/api', ReferralApiDefault)
+app.post('/api', ReferralApiDefault);
 
-app.get('/api/getlinksbyowner/:id', GetLinksByOwnerResponse)
+app.get('/api/getlinksbyowner/:id', GetLinksByOwnerResponse);
 
-app.get('/api/getownerdata/:id', GetOwnerDataResponse)
+app.get('/api/getownerdata/:id', GetOwnerDataResponse);
 
-app.post('/api/withdraw', WithdrawRewardAction)
+app.post('/api/withdraw', WithdrawRewardAction);
 
 // Stars (server contract parser)
-app.get('/api/getstarlist', GetAllStars)
+app.get('/api/getstarlist', GetAllStars);
 
-app.post('/api/updatestars', UpdateAllStars)
+app.post('/api/updatestars', UpdateAllStars);
 
-app.post('/api/updateonestar/:id', UpdateOneStar)
+app.post('/api/updateonestar/:id', UpdateOneStar);
 
 // Admin panel
-app.get('/api/public/:project', GetProjectData)
+app.get('/api/public/:project', GetProjectData);
 
-app.post('/api/admin/requestdata', AdminDataRequest)
+app.post('/api/admin/requestdata', AdminDataRequest);
 
-app.post('/api/admin/savedata', AdminSaveData)
+app.post('/api/admin/savedata', AdminSaveData);
 
-app.post('/api/admin/getusers', GetAdminUserData)
+app.post('/api/admin/getusers', GetAdminUserData);
 
-app.post('/api/admin/updateusers', AdminUpdateUserData)
+app.post('/api/admin/updateusers', AdminUpdateUserData);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}...`);
@@ -74,4 +93,3 @@ app.listen(port, () => {
 
 // InitGameIoServer()
 StartWatchingTimer();
-
