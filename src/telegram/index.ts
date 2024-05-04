0ac7b02d6f3c905f150ba0f8b9e2dd9abb75b53e
leoyoung02 <flarelight0125@gmail.com>
2024-05-04 10:12:59 +0500

    Telegram bot update

--- src/telegram/index.ts
+++ src/telegram/index.ts
@@ -2,21 +2,27 @@ import { Telegraf, Markup } from 'telegraf';
 require('dotenv').config();
 
 export async function InitTelegramClient() {
-  const tg_token = process.env.TELEGRAM_API_TOKEN;
-  console.log("Token: ", tg_token);
-
-  if (!tg_token) return;
-  const bot = new Telegraf(tg_token);
-
-  bot.command('start', (ctx) => {
-    ctx.reply(
-      'Press btn to start game',
-      Markup.keyboard([
-        Markup.button.webApp(
-          'Start vorpal game',
-          process.env.TELEGRAM_CLIENT_URL || '',
-        ),
-      ]),
-    );
-  });
+  return new Promise((resolve, reject) => {
+    const tg_token = process.env.TELEGRAM_API_TOKEN;
+    console.log("Token: ", tg_token);
+  
+    if (!tg_token) {
+      reject("Api token not found");
+      return;
+    }
+    
+    const bot = new Telegraf(tg_token);
+    
+    bot.command('start', (ctx) => {
+      ctx.reply(
+        'Press btn to start game',
+        Markup.keyboard([
+          Markup.button.webApp(
+            'Start vorpal game',
+            process.env.TELEGRAM_CLIENT_URL || '',
+          ),
+        ]),
+      );
+    });
+  })
 }
