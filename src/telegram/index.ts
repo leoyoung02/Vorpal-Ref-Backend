2199d71cc65a8e64891683b6a4cc67b9439d6797
leoyoung02 <flarelight0125@gmail.com>
2024-05-04 10:21:20 +0500

    Telegram bot testing

--- src/telegram/index.ts
+++ src/telegram/index.ts
@@ -10,7 +10,7 @@ export async function InitTelegramClient() {
       reject("Api token not found");
       return;
     }
-    
+
     const bot = new Telegraf(tg_token);
     
     bot.command('start', (ctx) => {
@@ -25,4 +25,36 @@ export async function InitTelegramClient() {
       );
     });
   })
+}
+
+export class TelegramBotServer {
+  tg_token: string;
+  bot: Telegraf;
+   
+  constructor() {
+    const token = process.env.TELEGRAM_API_TOKEN;
+    if (!token) {
+      throw new Error("Telegram token not found")
+    }
+    this.tg_token = token;
+    this.bot = new Telegraf(this.tg_token);
+  }
+
+  public start() {
+    this.bot.command('start', (ctx) => {
+      ctx.reply(
+        'Press btn to start game',
+        Markup.keyboard([
+          Markup.button.webApp(
+            'Start vorpal game',
+            process.env.TELEGRAM_CLIENT_URL || '',
+          ),
+        ]),
+      );
+    });
+  }
+
+  public stop() {
+    this.bot.stop();
+  }
 }
--- src/tests/telegramBotTest.ts
+++ src/tests/telegramBotTest.ts
@@ -1,9 +1,14 @@
-import { InitTelegramClient } from "../telegram";
+import { InitTelegramClient, TelegramBotServer } from "../telegram";
 require('dotenv').config();
 
 console.log("Telergram bot testing...");
-InitTelegramClient().then((res) => {
+
+/* InitTelegramClient().then((res) => {
     console.log("Finished: ", res)
 }).catch((e) => {
     console.log("Error found: ", e)
-}); 
+}); */
+
+const tgServer = new TelegramBotServer();
+
+tgServer.start();
