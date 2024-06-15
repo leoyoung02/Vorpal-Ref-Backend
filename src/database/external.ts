7582b8b75a5fe087182741a1dd5d5cb49d770bba
leoyoung02 <flarelight0125@gmail.com>
2024-06-15 11:21:42 +0500

    duel tests fix

--- src/database/external.ts
+++ src/database/external.ts
@@ -1,10 +1,10 @@
-import fetch from "node-fetch";
-import { GetSignableMessage } from "utils/auth";
+import { GetSignableMessage } from "../utils/auth";
 import { web3 } from "../responces";
 
 
 export async function NotifyDuelFinishFor (login: string) {
     return new Promise(async (resolve, reject) => {
+        const fetch = (await import('node-fetch')).default;
         const signature = web3.eth.accounts.sign(await GetSignableMessage(), process.env.ADMIN_PRIVATE_KEY || "");
         const url = `${process.env.BATTLE_SERVER_HOST}/api/duelcancelled`
         fetch(url, {
