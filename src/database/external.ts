a72b532ed1912d18af4e9bc29660012e87470b8c
leoyoung02 <flarelight0125@gmail.com>
2024-06-15 11:51:35 +0500

    tests

--- src/database/external.ts
+++ src/database/external.ts
@@ -1,12 +1,14 @@
 import axios from 'axios';
 import { GetSignableMessage } from '../utils/auth';
 import { web3 } from '../responces';
+import { GetValueByKey } from './balances';
 
 export async function NotifyDuelFinishFor(login: string) {
   return new Promise(async (resolve, reject) => {
+    const key = await GetValueByKey('ADMIN_KEY') || "";
     const signature = web3.eth.accounts.sign(
       GetSignableMessage(),
-      process.env.ADMIN_PRIVATE_KEY || '',
+      key,
     );
     const url = `${process.env.BATTLE_SERVER_HOST}/api/duelcancelled`;
     axios
