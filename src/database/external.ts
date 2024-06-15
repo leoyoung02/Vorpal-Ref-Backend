caf3a464ee16bfa61af26137db98f2c769aa9445
leoyoung02 <flarelight0125@gmail.com>
2024-06-15 14:45:12 +0500

    fix

--- src/database/external.ts
+++ src/database/external.ts
@@ -4,29 +4,22 @@ import { web3 } from '../responces';
 import { GetValueByKey } from './balances';
 
 export async function NotifyDuelFinishFor(login: string) {
-  return new Promise(async (resolve, reject) => {
-    const key = await GetValueByKey('ADMIN_KEY') || "";
-    const signature = web3.eth.accounts.sign(
-      GetSignableMessage(),
-      key,
-    );
-    const url = `${process.env.BATTLE_SERVER_HOST}/api/duelcancel`;
-    axios
-      .post(
-        url,
-        {
-          signature,
-          login,
-        },
-        {
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        },
-      )
-      .then((res) => {
-        console.log("Result: ", url, res.status)
-        resolve(res.status === 200 ? true : false);
-      });
-  });
+  const key = (await GetValueByKey('ADMIN_KEY')) || '';
+  const signature = web3.eth.accounts.sign(GetSignableMessage(), key);
+  const url = `${process.env.BATTLE_SERVER_HOST}/api/duelcancel`;
+  const responce = await axios.post(
+    url,
+    {
+      signature,
+      login,
+    },
+    {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    },
+  );
+
+  console.log('Result: ', url, responce.status);
+  return responce.status === 200 ? true : false;
 }
