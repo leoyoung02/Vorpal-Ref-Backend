5aa5b7729bafe198757c52a62c30d980f7ff0630
leoyoung02 <flarelight0125@gmail.com>
2024-06-18 20:14:18 +0500

    message sending test

--- src/database/external.ts
+++ src/database/external.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import superagent from 'superagent';
 import { GetSignableMessage } from '../utils/auth';
 import { web3 } from '../responces';
 import { GetValueByKey } from './balances';
@@ -7,19 +7,24 @@ export async function NotifyDuelFinishFor(login: string) {
   const key = (await GetValueByKey('ADMIN_KEY')) || '';
   const signature = web3.eth.accounts.sign(GetSignableMessage(), key);
   const url = `${process.env.BATTLE_SERVER_HOST}/api/duelcancel`;
-  const responce = await axios.post(
-    url,
-    JSON.stringify({
+  try {
+    const responce =  await superagent.post(url)
+    .send({
       signature,
       login,
-    }),
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    },
-  );
-
-  console.log('Result: ', url, responce.status);
-  return responce.status === 200 ? true : false;
+    }).set('Accept', 'application/json')
+    .then(response => {
+      console.log(response.body); // обработка успешного ответа
+      return responce.status === 200 ? true : false;
+    })
+    .catch(error => {
+      console.error('Error:', error); // обработка ошибки
+      return false;
+    });
+    console.log('Result: ', url, responce.status);
+    return responce.status === 200 ? true : false;
+  } catch (e) {
+    console.log(e.message);
+    return false;
+  }
 }
