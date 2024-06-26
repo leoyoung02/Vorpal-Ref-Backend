273dcca022749b2be674817a63291f4835307369
leoyoung02 <flarelight0125@gmail.com>
2024-06-26 22:27:58 +0500

    multiline keyboard test

--- src/telegram/handlers/keyboard.ts
+++ src/telegram/handlers/keyboard.ts
@@ -24,65 +24,76 @@ export const MarkupKeyboard = () => {
 };
 
 export const InlineKeyboard = (actions: string[], inviter?: string) => {
-  const keyboard: any[] = [];
+  const keyboard: any[][] = [];
+  let row: any[] = [];
+  
   actions.forEach((a) => {
     switch (true) {
       case a === 'duel':
-        keyboard.push({
+        row.push({
           text: duelText,
           callback_data: `duel`,
         });
         break;
       case a.indexOf('enterGame') > -1:
-          keyboard.push({
+        row.push({
             text: enterGameText,
             web_app: {"url": `${process.env.TELEGRAM_CLIENT_URL}`}
           });
         break;
       case a.indexOf('joinCommunity') > -1:
-          keyboard.push({
+        row.push({
             text: joinText,
             url:  communityTgUrl
           });
         break;
       case a.indexOf('duelConfirm') > -1:
-        keyboard.push({
+        row.push({
           text: duelConfirmText,
           web_app: {"url": `${process.env.TELEGRAM_CLIENT_URL}`}
         });
         break;
       case a.indexOf('duelRefuse') > -1:
-        keyboard.push({
+        row.push({
           text: duelRefuseText,
           callback_data: `${a.toLowerCase()}%${inviter || ""}`,
         });
         break;
         case a.indexOf('duelCancel') > -1:
-            keyboard.push({
+          row.push({
               text: "Cancel a duel",
               callback_data: `${a.toLowerCase()}%${inviter || ""}`,
             });
         break;
       case a === "transactions":
-        keyboard.push({
+        row.push({
           text: "Watch transactions",
           callback_data: `transactions`,
         });
         break;
       case a === "referrals":
-        keyboard.push({
+        row.push({
             text: "Watch referrals",
             callback_data: `referrals`,
         });
         break;
       default:
-        keyboard.push({
+        row.push({
           text: "Press action",
           callback_data: `${a.toLowerCase()}`,
         });
         break;
     }
+
+    if (row.length === 2) {
+      keyboard.push(row);
+      row = [];
+    }
   });
-  console.log({ inline_keyboard: [keyboard] });
-  return { inline_keyboard: [keyboard] };
+
+  if (row.length > 0) {
+    keyboard.push(row);
+  }
+
+  return { inline_keyboard: keyboard };
 };
