2b59eb5f5c8910fe6eb35f31fb9c33f6b4d748de
leoyoung02 <flarelight0125@gmail.com>
2024-06-26 20:56:24 +0500

    menu creation script

---
+++ src/cmdSetup.ts
@@ -0,0 +1,13 @@
+import { bot } from "./telegram/bot";
+
+try {
+    const cmd = bot.setMyCommands([
+        { command: '/start', description: 'Get started' },
+        { command: '/duel', description: 'Create duel' },
+        { command: '/referral', description: 'Referral info' }
+      ]);
+    console.log("Result: ", cmd)
+} catch (e: any) {
+    console.log("Error: ", e.message)
+}
+
